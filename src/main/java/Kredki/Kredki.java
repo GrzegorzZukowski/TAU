2f2cd06a2555be5e0e4495e878f4979874d0b0b2
Grzegorz <grzegorzzukowskigda@gmail.com>
2019-10-06 11:26:25 +0200

    Added Travis to project

---
+++ src/main/java/Kredki/Kredki.java
@@ -0,0 +1,4 @@
+package Kredki;
+
+public class Kredki {
+}
---
+++ src/test/java/Kredki/KredkiTest.java
@@ -0,0 +1,19 @@
+package Kredki;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static  org.junit.Assert.*;
+
+
+
+public class KredkiTest {
+
+    @Test
+    public void classImplementationTest(){
+        assertNotNull(new Kredki());
+    }
+
+
+
+}
