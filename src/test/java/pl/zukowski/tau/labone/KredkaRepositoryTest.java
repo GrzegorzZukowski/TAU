a7ee0842d5a6065d6a575c86f2c683d51783b295
grzegorzzukowskigda@gmail.com <grzegorzzukowskigda@gmail.com>
2019-10-15 11:04:56 +0200

    another read test

--- src/test/java/pl/zukowski/tau/labone/KredkaRepositoryTest.java
+++ src/test/java/pl/zukowski/tau/labone/KredkaRepositoryTest.java
@@ -1,10 +1,17 @@
 package pl.zukowski.tau.labone;
 
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import pl.zukowski.tau.labone.domain.Kredka;
 import pl.zukowski.tau.labone.service.KredkaRepository;
 
+
 import static org.junit.Assert.*;
+
+import java.util.NoSuchElementException;
+
+import org.junit.Rule;
 
 public class KredkaRepositoryTest  {
     //region List tests
@@ -64,7 +71,22 @@ public class KredkaRepositoryTest  {
 
         assertEquals("Objects are not equal", kredkaZielona, repo.read(1));
     }
+    
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
+    @Test
+    public void readShouldNotReturnObjectButThrowNoSuchElementException() throws NoSuchElementException {
 
+        Kredka kredkaZielona = new Kredka(1, "zielony");
+        KredkaRepository repo = new KredkaRepository();
+
+        repo.kredki.add(kredkaZielona);
+
+        exception.expect(NoSuchElementException.class);
+        exception.expectMessage("Nie ma takiej kredki w bazie");
+
+        assertNotNull(repo.read(2));
+    }
     //endregion
 }
